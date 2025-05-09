57d1930f79bb9bbaed06bd56eac4d8c8fee8f410
Roxann <rofaniry@gmail.com>
2025-05-09 14:20:29 +0300

    feat: sujet 1

---
+++ sujet1.js
@@ -0,0 +1,28 @@
+const getLastDelegation = (delegations) => {
+    let split = [];
+    let max;
+    let result;
+    
+    for(let i = 0; i < delegations.length; i++){
+        split.push(delegations[i].split(" ")) 
+        for (let j = 0; j < split[i].length; j++){
+            if (isNaN(split[i][j]) == false){
+                split[i][j] = +split[i][j]
+            }
+        }
+        if (i == 0){
+            max = split[i][1]/split[i][2]
+
+        } else if (i > 0){
+            split[i][1]/split[i][2]
+            if (max < split[i][1]/split[i][2]){
+                max = split[i][1]/split[i][2]
+            }
+        }
+        if(max  == split[i][1]/split[i][2]){
+            result = split[i][0]
+        }
+        
+    } 
+    return result
+}
