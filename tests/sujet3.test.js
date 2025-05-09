bd7569854cbcb0f25e75b518b8c51d1b9ac0dc29
Yoan <yoandiny14@gmail.com>
2025-05-09 15:42:04 +0300

    node module deleted

--- tests/sujet3.test.js
+++ tests/sujet3.test.js
@@ -1,27 +1,27 @@
 import {describe, it} from 'mocha'
 import { expect } from 'chai'
-import { reduceDirections } from '../sujet_4thkuy.js'
+import { reduceDirections } from '../sujet3.js'
 
 
 describe('Function that delete the useless path', ()=>{
     it('Should return the non useless path', ()=>{
-        expect(reduceDirections(['NORD', 'SUD', 'SUD', 'EST', 'OUEST', 'NORD', 'OUEST'])).to.equal(['OUEST']);
-        expect(reduceDirections(['NORD', 'OUEST', 'SUD', 'NORD'])).to.equal(['OUEST', 'NORD']);
+        expect(reduceDirections(['NORD', 'SUD', 'SUD', 'EST', 'OUEST', 'NORD', 'OUEST'])).to.deep.equal(['OUEST']);
+        expect(reduceDirections(['NORD', 'OUEST', 'SUD', 'NORD'])).to.deep.equal(['OUEST', 'NORD']);
     });
 
     it('Should return an empty array if all of the directions are useless', ()=>{
-        expect(reduceDirections(['NORD', ' SUD', 'EST', 'OUEST'])).to.equal([]);
+        expect(reduceDirections(['NORD', ' SUD', 'EST', 'OUEST'])).to.deep.equal([]);
         expect(reduceDirections(['NORD', 'SUD']));
         expect(reduceDirections(['EST', 'OUEST']));
     });
 
     it('Should return the same array if ther is no useless direction', ()=>{
-        expect(reduceDirections(['NORD', 'OUEST'])).to.equal(['NORD', 'OUEST']);
-        expect(reduceDirections(['EST', 'SUD'])).to.equal(['EST', 'SUD']);
+        expect(reduceDirections(['NORD', 'OUEST'])).to.deep.equal(['NORD', 'OUEST']);
+        expect(reduceDirections(['EST', 'SUD'])).to.deep.equal(['EST', 'SUD']);
     });
 
     it('Should return an empty array if the array in param is empty', ()=>{
-        expect(reduceDirections([])).to.equal([]);
+        expect(reduceDirections([])).to.deep.equal([]);
 
     })
 
